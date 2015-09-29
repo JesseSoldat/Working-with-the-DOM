07ea5e6aa9c12988625bc4f5d00eedaaccbd8498
Jesse Soldat <jsoldat@hotmail.com>
2015-09-29 15:11:50 -0400

    Separated my function and my event listener

--- main.js
+++ main.js
@@ -5,32 +5,48 @@ var box3 = document.querySelector('#operator')
 var calcBtn = document.querySelector('#calculateBtn');
 var answ = document.querySelector('#answer')
 
-// add an eventListener for when the user clicks the button
-calcBtn.addEventListener('click', function(firstNum, secondNum, operator) {
-   var firstNum = box1.value;
-   var secondNum = box2.value;
-   var firstNum1 = parseInt(firstNum);
-   var secondNum2 = parseInt(secondNum);
-   var operator = box3.value;
+// calculate function
+var calculate = function() {
+
+  var firstNum = box1.value;
+  var secondNum = box2.value;
+  var firstNum1 = parseInt(firstNum);
+  var secondNum2 = parseInt(secondNum);
+  var operator = box3.value;
    // console.log(firstNum);
    // console.log(secondNum);
    // console.log(operator);
-       
+
+   
         if(operator == '+') {
           console.log(firstNum1 + secondNum2);
-            // var calc = firstNum1 + secondNum2;
-            
+            var calc = firstNum1 + secondNum2;
+            return calc;  
         }
         else if(operator == '-') {
           console.log(firstNum1 - secondNum2);
+          var calc = firstNum1 - secondNum2;
+            return calc;  
         }
         else if(operator == '*') {
           console.log(firstNum1 * secondNum2);
+          var calc = firstNum1 * secondNum2;
+            return calc;  
         }
         else if(operator == '/') {
           console.log(firstNum1 / secondNum2);
+          var calc = firstNum1 / secondNum2;
+            return calc;  
         }
         else 
-          console.log('check what you entered');
-        
-}); //addEventListener
+          console.log('check what you entered');    
+}; 
+// add an eventListener for when the user clicks the button
+ calcBtn.addEventListener('click', calculate);
+
+ 
+
+  
+
+
+   
